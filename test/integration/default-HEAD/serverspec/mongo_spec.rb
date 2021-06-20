23a9744029969a83a37890afc3140807104ec534
juju4 <juju4@users.noreply.github.com>
2021-06-20 09:09:51 -0400

    use different kitchen test suite default-HEAD instead of env variable

---
+++ test/integration/default-HEAD/serverspec/mongo_spec.rb
@@ -0,0 +1,12 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe process("mongod") do
+  it { should be_running }
+end
+
+describe port(27018) do
+  it { should be_listening.with('tcp') }
+end
