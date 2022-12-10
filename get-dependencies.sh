37ba861815f2923085c7ffac26d7a729e9798653
juju4 <juju4@users.noreply.github.com>
2022-12-10 15:19:37 +0000

    ci(default-HEAD): add geerlingguy.nodejs role +get-dependencies

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,6 +10,7 @@ rolesdir=$(dirname $0)/..
 fi
 
 [ ! -d $rolesdir/geerlingguy.docker ] && git clone https://github.com/geerlingguy/ansible-role-docker.git $rolesdir/geerlingguy.docker
+[ ! -d $rolesdir/geerlingguy.nodejs ] && git clone https://github.com/geerlingguy/ansible-role-nodejs.git $rolesdir/geerlingguy.nodejs
 [ ! -d $rolesdir/geerlingguy.postgresql ] && git clone https://github.com/geerlingguy/ansible-role-postgresql.git $rolesdir/geerlingguy.postgresql
 
 ## don't stop build on this script return code
